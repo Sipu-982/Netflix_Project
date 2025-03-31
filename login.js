3e4d4cbb460134faae9f7a4d72138a29b635108c
Sipu-982 <sipurana38@gmail.com>
2025-03-31 14:12:10 +0530

    JS Code Submitted

--- login.js
+++ login.js
@@ -16,3 +16,28 @@ togglePassword.addEventListener('click', () => {
     toggleIcon.classList.add('fa-eye');
   }
 });
+
+// login form validation
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form= document.getElementById("loginForm");
+  form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+
+    // input fields
+    const userid=document.getElementById("userId").value.trim();
+    const password=document.getElementById("password").value.trim();
+   
+    if(userid==="" || password===""){
+      alert("All fields are mandatory")
+    }
+    else if(password.length>7){
+      alert("password must be 6 digits");
+    }
+    else{
+      alert("Login successfull!");
+      form.reset();
+    }
+  })
+
+});
